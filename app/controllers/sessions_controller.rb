2fdf69ccbc7027f20c69a180d1b4c5432d544623
Sebastian Sanchez <sebastian@artxcode.io>
2021-04-12 15:32:07 -0400

    built out all views needed at the end of phase 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController 
 
-    before_action :require_logged_in!, only: [:destroy]
+    # before_action :require_logged_in!, only: [:destroy]
 
     def new 
         render :new
     end 
 
     def create
+        debugger
         @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-
         if @user 
+            login!(@user)
             redirect_to subs_url
         else 
             flash[:errors] = @user.errors.full_messages
